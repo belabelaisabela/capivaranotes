6125f3865cb2f16417e695d37666bbea1e97b7dd
belabelaisabela <isabelagualberto7@gmail.com>
2023-09-27 00:52:02 -0400

    Initial commit

---
+++ js/db.js
@@ -0,0 +1,78 @@
+import { openDB } from "idb";
+
+let db;
+async function criarDB(){
+    try {
+        db = await openDB('banco', 1, {
+            upgrade(db, oldVersion, newVersion, transaction){
+                switch  (oldVersion) {
+                    case 0:
+                    case 1:
+                        const store = db.createObjectStore('anotacao', {
+                            keyPath: 'titulo'
+                        });
+                        store.createIndex('id', 'id');
+                        console.log("banco de dados criado!");
+                }
+            }
+        });
+        console.log("banco de dados aberto!");
+    }catch (e) {
+        console.log('Erro ao criar/abrir banco: ' + e.message);
+    }
+}
+
+window.addEventListener('DOMContentLoaded', async event =>{
+    criarDB();
+    document.getElementById('btnCadastro').addEventListener('click', adicionarAnotacao);
+    document.getElementById('btnCarregar').addEventListener('click', buscarTodasAnotacoes);
+});
+
+async function buscarTodasAnotacoes(){
+    if(db == undefined){
+        console.log("O banco de dados está fechado.");
+    }
+    const tx = await db.transaction('anotacao', 'readonly');
+    const store = await tx.objectStore('anotacao');
+    const anotacoes = await store.getAll();
+    if(anotacoes){
+        const divLista = anotacoes.map(anotacao => {
+            return `<div class="item">
+                    <p>Anotação</p>
+                    <p>${anotacao.titulo} - ${anotacao.data} </p>
+                    <p>${anotacao.categoria}</p>
+                    <p>${anotacao.descricao}</p>
+                   </div>`;
+        });
+        listagem(divLista.join(' '));
+    }
+}
+
+async function adicionarAnotacao() {
+    let titulo = document.getElementById("titulo").value;
+    let descricao = document.getElementById("descricao").value;
+    let data = document.getElementById("data").value;
+    let categoria = document.getElementById("categoria").value;
+    const tx = await db.transaction('anotacao', 'readwrite')
+    const store = tx.objectStore('anotacao');
+    try {
+        await store.add({ titulo: titulo, descricao: descricao, data: data, categoria:categoria });
+        await tx.done;
+        limparCampos();
+        console.log('Registro adicionado com sucesso!');
+    } catch (error) {
+        console.error('Erro ao adicionar registro:', error);
+        tx.abort();
+    }
+}
+
+function limparCampos() {
+    document.getElementById("titulo").value = '';
+    document.getElementById("descricao").value = '';
+    document.getElementById("data").value = '';
+    document.getElementById("categoria").value = '';
+}
+
+function listagem(text){
+    document.getElementById('resultados').innerHTML = text;
+}
